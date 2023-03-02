064971bed09ffbd42c7e60eae7d2485c934a67df
DongJu-Na <ehdeld123@naver.com>
2023-03-02 17:19:27 +0900

    init
    init

---
+++ src/js/chrome/background.js
@@ -0,0 +1,7 @@
+'use strict';
+import {SETTINGS} from './common';
+
+
+console.log(SETTINGS);
+
+
---
+++ src/js/chrome/common.js
@@ -0,0 +1,9 @@
+/* Settings */
+let SETTINGS = {
+    "INTERVAL_CHECK_DEFAULT"  : 1000,
+    "INTERVAL_SAVE_STORAGE_DEFAULT" : 5000,
+}
+
+export default {
+    SETTINGS
+}
